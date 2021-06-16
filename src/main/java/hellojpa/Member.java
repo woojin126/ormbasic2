4934056f1496aac946d151cbddeb456e52d9f95c
woojin126 <woojin126@naver.com>
2021-06-16 19:46:18 +0900

    MemberEntity

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,24 @@
+package hellojpa;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "TEAM_ID")
+    private Long teanId;
+}
