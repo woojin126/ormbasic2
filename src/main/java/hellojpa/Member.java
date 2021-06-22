c52fd0a5f73c7eb173ce517e94e7da12feb2086d
woojin126 <woojin126@naver.com>
2021-06-22 15:42:53 +0900

    Embedded 값 타입 컬렉션으로

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -4,6 +4,11 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter @Setter
@@ -15,6 +20,44 @@ public class Member extends BaseEntity {
 
     @Column(name = "USERNAME")
     private String username;
+
+    //기간 period
+    @Embedded
+    private Period workPeriod;
+
+    //주소
+    @Embedded
+    private Address homeAddress;
+
+    /**
+     * 값 타입 컬렉션
+     * 1.값 타입을 하나 이상 저장할 때 사용
+     * 2.@ElementCollection, @CollectionTable 사용
+     * 3.데이터베이스는 컬렉션을 같은 테이블에 저장할 수 없음
+     * 컬렉션을 저장하기 위한 별도의 테이블이 필요
+     */
+    @ElementCollection //값 타입 컬렉션입니다 
+    @CollectionTable(name ="FAVORITE_FOOD",joinColumns = @JoinColumn(name = "MEMBER_ID")) //컬렉션 테이블 생성,외래키로지정 만들어질 테이블에
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name ="ADDRESS",joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    private List<Address> addressHistory = new ArrayList<>();
+
+    /**
+     * @AttributeOverride:속성 재정의
+     * 1.한 엔티티에서 같은 값 타입을 사용하려면?
+     * 2.컬럼 명이 중복됨
+     * 3. @AttributeOverrides, @AttributeOverride 를 사용해서 컬럼 명 속성을 재정의
+     */
+   /* @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="city", column = @Column(name = "WORK_CITY")), //디비컬럼은 따로매핑
+            @AttributeOverride(name="street", column = @Column(name = "WORD_STREET")),
+            @AttributeOverride(name="zipcodes", column = @Column(name = "WORK_ZIPCODES"))
+    })
+    private Address workAddress;//같은 주소가있다면? 위에처럼 새로 오버라이드*/
 
  /*   @ManyToOne//oneToMany쪽이 주인
     @JoinColumn(name = "TEAM_ID",insertable = false,updatable = false)
@@ -31,8 +74,9 @@ public class Member extends BaseEntity {
      * LAZY
      *1.지연로딩을 사용시 연관된 것을 프록시로 가져옴!
      *2.team.getName() // 실제 team을 사용하는 시점에 초기화(DB조회가 일어남)
-     *2.Member와 Team을 따로 자주 사용하는경우( 맴버만 조회할 경우가 많은경우) 사용
-     *
+     *3.Member와 Team을 따로 자주 사용하는경우( 맴버만 조회할 경우가 많은경우) 사용
+     *4.@ManyToOne , @OneToOne은 기본이 즉시 로딩이므로 지연로딩으로 변경
+     * 
      * EAGER
      *1.MEMBER와 Team을 자주 함께 사용할때. (함께조회하도록해버림) ==즉시로딩
      *2.엔 한방에 outerJoin 해서 다가져오기떄문에 proxy가 필요없음
