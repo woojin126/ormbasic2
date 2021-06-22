1f48d3b43c27d5a2a884da9b241ccbc39aadeef0
woojin126 <woojin126@naver.com>
2021-06-22 15:44:56 +0900

    기본값, 객체값 의 == 의 차이점,

--- src/main/java/hellojpa/ValueMain.java
+++ src/main/java/hellojpa/ValueMain.java
@@ -1,2 +1,25 @@
-package hellojpa;public class ValueMain {
+package hellojpa;
+
+public class ValueMain {
+
+    public static void main(String[] args){
+
+        int a=10;
+        int b=10;
+
+        System.out.println("a == b " + (a==b)); //기본타입은 true (내부에 값이 10 10 )
+
+        Address homeAddress1 = new Address("city", "street", "zipcodes");
+        Address homeAddress2 = new Address("city", "street", "zipcodes");
+
+        System.out.println("adress1 == adress2 " + (homeAddress1==homeAddress2)); //객체는(참조값을 비교하니) false
+        System.out.println("adress1 == adress2 " + (homeAddress1==homeAddress2)); //객체는(참조값을 비교하니) false
+        System.out.println("adress1 equals adress2 " + (homeAddress1.equals(homeAddress2))); //기본값을 제외한 -> embedded , 값타입,등등은 equals 로 비교해라!
+
+        /**
+         * 갑 타입 비교
+         *
+         * 1.동일성(identity) 비교: 인스턴스의 참조 값을 비교
+         */
+    }
 }
