e64c24ac03cb68d3b936c351e6bb49889d26b62d
woojin126 <woojin126@naver.com>
2021-06-22 15:41:08 +0900

    Period 시작시간, 마지막시간  @Embeddable

--- src/main/java/hellojpa/Period.java
+++ src/main/java/hellojpa/Period.java
@@ -1,2 +1,28 @@
-package hellojpa;public class Period {
+package hellojpa;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+import java.time.LocalDateTime;
+
+@Embeddable//값타입이라는 의미
+@Getter
+@Setter
+public class Period {
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+
+    public Period(){
+
+    }
+
+    public Period(LocalDateTime startDate, LocalDateTime endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    //여기서 메서드작정해도됨
+
 }
