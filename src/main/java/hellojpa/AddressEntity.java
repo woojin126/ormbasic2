e07f3da3d16547a00fc126a25d2730d5be2ece1c
woojin126 <woojin126@naver.com>
2021-06-24 15:32:28 +0900

    AddressEntity

--- src/main/java/hellojpa/AddressEntity.java
+++ src/main/java/hellojpa/AddressEntity.java
@@ -1,2 +1,32 @@
-package hellojpa;public class AddressEntity {
+package hellojpa;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "ADDRESS")
+public class AddressEntity { //엔 엔티티
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private Address address; //엔티티로 래핑 (엔 값타입)
+
+    public AddressEntity(){
+
+    }
+    public AddressEntity(Address address) {
+        this.address = address;
+    }
+
+    public AddressEntity(String city, String street, String zipcode) {
+        this.address=new Address(city,street,zipcode);
+    }
 }
